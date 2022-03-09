092b96b806fd0f65c9807e331d808526c729e369
ebimo21 <ebi4jah15@gmail.com>
2022-03-09 16:34:25 +0100

    variable length edith

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('viewpassword');
+            $table->string('name', 150);
+            $table->string('email', 150)->unique();
+            $table->timestamp('email_verified_at', 150)->nullable();
+            $table->string('password', 150);
+            $table->string('viewpassword', 150);
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->foreignId('current_team_id', 150)->nullable();
+            $table->string('profile_photo_path', 150)->nullable();
             $table->timestamps();
         });
     }
